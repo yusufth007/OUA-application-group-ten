46cd536f7155efb66149b6923a7643ef65018693
Ege Kula <egekula3@gmail.com>
2024-08-01 19:27:37 +0300

    +Calculator LastCommit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
-import 'package:davetioapp/constants/theme.dart';
-import 'package:davetioapp/ui/login_page.dart';
-import 'package:davetioapp/ui/signup_page.dart';
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
+import 'package:davetioapp/ui/calculator_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-
   runApp(const MyApp());
 }
 
@@ -17,15 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-        "/loginPage" : (context) => LoginPage(),
-        "/signupPage" : (context) => SignUpPage(),
-      },
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      themeMode: ThemeMode.system,
-      home: LoginPage(),
+      theme: ThemeData.dark(),
+      home: const Calculator(),
     );
   }
 }
